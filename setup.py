6429be63458bfdedcce92c2545b79d850ebc11c3
Josh Fyne <josh.fyne@essencedigital.com>
2015-11-10 14:49:43 -0500

    First pass at python version lib essence auth

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='Essence Auth',
+    version='0.1',
+    description='Library for Essence authentication',
+    author='Josh Fyne',
+    author_email='josh.fyne@essencedigital.com',
+    url='https://github.com/essence-tech/lib-essence-auth',
+    packages=['essenceauth',],
+    install_requires=['requests'],
+    )
