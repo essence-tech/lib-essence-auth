e7d835ef50804e417069afe2d3b5ae1543abbc89
Josh Fyne <josh.fyne@essencedigital.com>
2016-12-28 10:54:13 -0500

    Python test skeleton

---
+++ client_test.py
@@ -0,0 +1,13 @@
+"""Test auth service client."""
+
+import unittest
+
+
+class TestClient(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
