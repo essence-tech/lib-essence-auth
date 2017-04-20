0f7897c4c80d202aa2d4d6853b7729fd44c58f6a
Josh Fyne <josh.fyne@essencedigital.com>
2017-04-20 09:56:30 -0400

    Adding missing init

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
     name='Essence Auth',
-    version='2.0.7',
+    version='2.0.8',
     description='Library for the Essence authentication service.',
     author='Josh Fyne',
     author_email='josh.fyne@essencedigital.com',
